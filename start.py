cf408cb2528626780fdad73626dbf20e6ecf2a29
egcash <85733533+egcash@users.noreply.github.com>
2024-03-01 03:43:07 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
+
 from g4f import set_cookies
 
 set_cookies(".bing.com", {
-    "_U":  "1BJKrxJSYb_YE84Wl6aSsND-uQ_MsiZT1IFjQkseVxDuhpWMN5cREP4wRq>
+    "_U":  "1BJKrxJSYb_YE84Wl6aSsND-uQ_MsiZT1IFjQkseVxDuhpWMN5cREP4wRqWCUzVzFgTjGwzDQch8yeVEuXn6SGNJrvxKwFEMb40nr95q9DNhibNJKXX__E03dURgBT-O6g1_oKu1oveDs-Mp3Nim0EsgCqI8Fj4zsHts2bHsWf9_AewfifC4RkWzNt-4Gxs1d6zq4CUBjxzvJefrQDuxo2LV86mqJ9Zst1pZt_2jiuIQ"
 })
 
 #from g4f.gui import run_gui
